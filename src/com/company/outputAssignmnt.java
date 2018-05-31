d4c0b8f65d8f39fe1e04b2dcd6c26ac81678f3b9
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 16:22:37 -0400

    first commit

---
+++ src/com/company/outputAssignmnt.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class outputAssignmnt {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+
+        String firstName;
+        int age;
+        String height;
+        double gpa;
+
+        System.out.print( "What is your first name? " );
+        firstName = keyboard.next();
+
+        System.out.print( "How old are you? " );
+        age = keyboard.nextInt();
+
+        System.out.print( "How tall are you? " );
+        height = keyboard.next();
+
+        System.out.print( "What is your GPA? " );
+        gpa = keyboard.nextDouble();
+
+    }
+}
